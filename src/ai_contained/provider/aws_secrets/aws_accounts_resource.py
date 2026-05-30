c6f31be9302abb1de5970a8a5295c270b91d9e94
Ryan Kraay <6840071+ryan-kraay@users.noreply.github.com>
2026-05-30 15:25:55 +0200

    chore: add aws_account_resource implementation

--- src/ai_contained/provider/aws_secrets/aws_accounts_resource.py
+++ src/ai_contained/provider/aws_secrets/aws_accounts_resource.py
@@ -17,7 +17,24 @@ class AwsAccountResourceEntry:
 
 class AwsAccountsResource:
     def __init__(self, accounts: Accounts, auth_read: AwsAuthTool, auth_write: AwsAuthTool) -> None:
-        raise NotImplementedError
+        self._accounts = accounts
+        self._auth_read = auth_read
+        self._auth_write = auth_write
+
+    def _access_status(self, account_id: AwsAccountId, auth: AwsAuthTool, aws_profile: str | None) -> AccessStatus:
+        if aws_profile is None:
+            return AccessStatus.UNSUPPORTED
+        elif auth.is_authorized(account_id):
+            return AccessStatus.AUTHORIZED
+        return AccessStatus.REQUIRES_AUTH
 
     def get(self) -> dict[AwsAccountId, AwsAccountResourceEntry]:
-        raise NotImplementedError
+        return {
+            account.account_id: AwsAccountResourceEntry(
+                name=account.name,
+                trust_groups=account.trust_groups,
+                read_only=self._access_status(account.account_id, self._auth_read, account.read_profile),
+                read_write=self._access_status(account.account_id, self._auth_write, account.write_profile),
+            )
+            for account in self._accounts.all_accounts()
+        }
