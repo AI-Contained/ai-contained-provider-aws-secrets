75ee91f1debd4d029e56b16e6c046871ef4a8f4f
Ryan Kraay <6840071+ryan-kraay@users.noreply.github.com>
2026-05-30 14:23:37 +0200

    chore: improve tests

--- tests/test_aws_auth_tool.py
+++ tests/test_aws_auth_tool.py
@@ -1,3 +1,4 @@
+import pytest
 from assertpy import assert_that
 
 from ai_contained.provider.aws_secrets.aws_auth_tool import AwsAuthTool
@@ -15,15 +16,15 @@ def describe_AwsAuthTool():
             tool.authorize("123456789012", Role.READ_ONLY)
             assert_that(tool.is_authorized("123456789012", Role.READ_ONLY)).is_true()
 
-        def it_read_only_does_not_grant_read_write() -> None:
+        @pytest.mark.parametrize("authorized,checked", [
+            (Role.READ_ONLY,  Role.READ_WRITE),
+            (Role.READ_WRITE, Role.READ_ONLY),
+        ])
+        def it_does_not_grant_other_role(authorized: Role, checked: Role) -> None:
             tool = AwsAuthTool()
-            tool.authorize("123456789012", Role.READ_ONLY)
-            assert_that(tool.is_authorized("123456789012", Role.READ_WRITE)).is_false()
-
-        def it_read_write_does_not_grant_read_only() -> None:
-            tool = AwsAuthTool()
-            tool.authorize("123456789012", Role.READ_WRITE)
-            assert_that(tool.is_authorized("123456789012", Role.READ_ONLY)).is_false()
+            tool.authorize("123456789012", authorized)
+            assert_that(tool.is_authorized("123456789012", authorized)).is_true()
+            assert_that(tool.is_authorized("123456789012", checked)).is_false()
 
         def it_returns_false_after_revoke() -> None:
             tool = AwsAuthTool()
